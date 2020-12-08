00ea9dc785402d8b8976931ff3c831ba367f7aa1
Gabe <gabe@maentz.net>
2020-12-08 12:56:10 -0500

    Update main.tf

--- solution/main.tf
+++ solution/main.tf
@@ -1,42 +1,8 @@
-#
-# DO NOT DELETE THESE LINES UNTIL INSTRUCTED TO!
-#
-# Your AMI ID is:
-#
-#     ami-074a777d5cae10210
-#
-# Your subnet ID is:
-#
-#     subnet-09abf0e2107aa70f3
-#
-# Your VPC security group ID is:
-#
-#     sg-0d883a53e09ab3d8c
-#
-# Your Identity is:
-#
-#     terraform-census-ant
-#
-
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
   region     = var.region
 }
-
-# resource "aws_instance" "web" {
-#   ami = (var.ubuntu_version == "18") ? data.aws_ami.ubuntu_18_04.image_id : data.aws_ami.ubuntu_16_04.image_id
-#   instance_type = "t2.micro"
-
-#   subnet_id              = var.subnet_id
-#   vpc_security_group_ids = var.vpc_security_group_ids
-
-#   tags = {
-#     "Identity"    = var.identity
-#     "Name"        = "Gabe EC2"
-#     "Environment" = "Training"
-#   }
-# }
 
 module "server" {
   source                 = "./server"
