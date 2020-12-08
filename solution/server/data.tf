b5b239f20bfb32614e387d1a7495c649c7ec5b1b
Gabe <gabe@maentz.net>
2020-12-08 12:59:00 -0500

    Create data.tf

---
+++ solution/server/data.tf
@@ -0,0 +1,21 @@
+data "aws_ami" "ubuntu_16_04" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"]
+}
+
+data "aws_ami" "ubuntu_18_04" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"]
+}
